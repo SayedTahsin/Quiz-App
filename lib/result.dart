55c36860b3b70637d0d148e0c00edb643b3a1db2
SayedTahsin <mail.tahsin99@gmail.com>
2023-03-05 01:11:33 +0600

    3/5/23 : 1:11 AM

--- lib/result.dart
+++ lib/result.dart
@@ -1,9 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class Result extends StatelessWidget {
   const Result({super.key});
